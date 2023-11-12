856c7c76c145d15246f4faf64cf6c6c97b1f1293
dazheng6 <dazheng6@s.sfusd.edu>
2023-11-12 15:01:57 -0800

    Finished Brick and Brick Spawning

--- Scripts/brick_spawner.gd
+++ Scripts/brick_spawner.gd
@@ -9,7 +9,7 @@ const ROWS = 6
 @onready var ui = $"../UI" as UI
 
 @export var brick_scene: PackedScene
-@export var margin: Vector2 = Vector2(8, 8)
+@export var margin: Vector2 = Vector2(8,8)
 @export var spawn_start: Marker2D
 
 var brick_count = 0
@@ -18,8 +18,6 @@ var brick_count = 0
 func _ready():
 	spawn()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func spawn():
 	var test_brick = brick_scene.instantiate() as Brick
 	add_child(test_brick)
@@ -27,19 +25,22 @@ func spawn():
 	test_brick.queue_free()
 	
 	var row_width = brick_size.x * COLUMNS + margin.x * (COLUMNS - 1)
-	var spawn_position_x = ((-row_width + brick_size.x + margin.x) / 2) + 130
+	var spawn_position_x = (-row_width  + brick_size.x  + margin.x) / 2 
 	var spawn_position_y = spawn_start.position.y
 	
 	for i in ROWS:
 		for j in COLUMNS:
 			var brick = brick_scene.instantiate() as Brick
 			add_child(brick)
-			var x = spawn_position_x + j * (margin.x + brick.get_size().x ) + 400
-			var y = spawn_position_y + i * (margin.y + brick.get_size().y ) + 200
-			brick.set_position(Vector2(x, y ))
+			brick.set_level(ROWS - i)
+			var x = spawn_position_x + j * (margin.x + brick.get_size().x ) + 530
+			var y = spawn_position_y + i * (margin.y + brick.get_size().y ) + 100
+			brick.set_position(Vector2(x, y))
 			brick.brick_destroyed.connect(on_brick_destroyed)
-			
+			brick_count += 1
+
 func on_brick_destroyed():
 	brick_count -= 1
 	if brick_count == 0:
-		ball.stop_ball()
+		ball.reset_ball()
+		ui.on_level_won()
