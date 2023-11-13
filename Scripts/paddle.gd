2d719b31b9d92bb9a725d5f456a541af390db53e
dazheng6 <dazheng6@s.sfusd.edu>
2023-11-12 23:48:27 -0800

    Actually locked the y axis using global position . y

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -18,17 +18,15 @@ func _ready():
 	camera_rect = camera.get_viewport_rect()
 	half_paddle_width = collision_shape_2d.shape.get_rect().size.x / 2 * scale.x
 	set_use_custom_integrator(true)
-	
+
 func _integrate_forces(state):
 	var vel = state.get_linear_velocity()
 	state.set_linear_velocity(Vector2(0, vel.y))
-	
+
 func _physics_process(delta):
 	linear_velocity = speed * direction
-	position = Vector2(global_position.x, global_position.y)
-	linear_velocity = speed * direction
+	global_position.y += linear_velocity.y * delta
 
-func _process(delta):
 	var camera_start_x = camera.position.x - camera_rect.size.x / 2
 	var camera_end_x = camera_start_x + camera_rect.size.x
 	
@@ -36,23 +34,25 @@ func _process(delta):
 		global_position.x = camera_start_x + half_paddle_width
 	elif global_position.x + half_paddle_width > camera_end_x:
 		global_position.x = camera_end_x - half_paddle_width
-		
+
+	# Ensure the paddle stays at the specified y-coordinate
+	global_position.y = 620
 
 func _input(event):
 	if Input.is_action_pressed("left"):
 		direction = Vector2.LEFT
-	elif  Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right"):
 		direction = Vector2.RIGHT
 	else:
 		direction = Vector2.ZERO
-		
+
 	if direction != Vector2.ZERO && !is_ball_started:
 		ball.start_ball()
 		is_ball_started = true
-		
+
 func on_ball_lost():
 	is_ball_started = false
 	direction = Vector2.ZERO
-	
+
 func get_width():
 	return collision_shape_2d.shape.get_rect().size.x
